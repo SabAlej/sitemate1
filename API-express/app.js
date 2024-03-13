d8c1cdd0e3176fe91f9e7e46f423fed8695d265d
SabAlej <sabrinaacm@gmail.com>
2024-03-13 10:34:45 -0500

    get issues endpoint

--- API-express/app.js
+++ API-express/app.js
@@ -6,9 +6,18 @@ const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
+const data = fs.readFileSync('../data/data.json');
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('API works');
+});
+
+app.get('/issues', (req, res) => {
+  if (data) {
+    res.send(JSON.parse(data));
+  } else {
+    res.send('No data found');
+  }
 });
 
 app.listen(port, () => {
