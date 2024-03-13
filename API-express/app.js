b2de79c94fde63881476dbb1bc07fb19fca31bbc
SabAlej <sabrinaacm@gmail.com>
2024-03-13 10:53:12 -0500

    endpoints for update and delete

--- API-express/app.js
+++ API-express/app.js
@@ -43,6 +43,51 @@ app.post('/create', (req, res) => {
   });
 });
 
+app.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedIssue = req.body;
+  fs.readFile(dataFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(502).json({ message: 'Error reading data' });
+    } else {
+      const jsonData = JSON.parse(data);
+      const index = jsonData.findIndex(issue => issue.id === id);
+      jsonData[index] = updatedIssue;
+      fs.writeFile(dataFile, JSON.stringify(jsonData), err => {
+        if (err) {
+          console.error(err);
+          res.status(502).json({ message: 'Error writing data' });
+        } else {
+          res.json(updatedIssue);
+        }
+      });
+    }
+  });
+});
+
+app.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  fs.readFile(dataFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(502).json({ message: 'Error reading data' });
+    } else {
+      const jsonData = JSON.parse(data);
+      const index = jsonData.findIndex(issue => issue.id === id);
+      jsonData.splice(index, 1);
+      fs.writeFile(dataFile, JSON.stringify(jsonData), err => {
+        if (err) {
+          console.error(err);
+          res.status(502).json({ message: 'Error writing data' });
+        } else {
+          res.json({ message: 'Issue deleted' });
+        }
+      });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
