99d6c29223e2a39e5823ed72c619edd389e5c245
SabAlej <sabrinaacm@gmail.com>
2024-03-13 10:43:45 -0500

    modified get-issues, new endpoint create

--- API-express/app.js
+++ API-express/app.js
@@ -6,18 +6,41 @@ const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
-const data = fs.readFileSync('../data/data.json');
 
 app.get('/', (req, res) => {
   res.send('API works');
 });
 
 app.get('/issues', (req, res) => {
-  if (data) {
-    res.send(JSON.parse(data));
-  } else {
-    res.send('No data found');
-  }
+  fs.readFile(dataFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(502).json({ message: 'Error reading data' });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+app.post('/create', (req, res) => {
+  const newIssue = req.body;
+  fs.readFile(dataFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(502).json({ message: 'Error reading data' });
+    } else {
+      const jsonData = JSON.parse(data);
+      jsonData.push(newIssue);
+      fs.writeFile(dataFile, JSON.stringify(jsonData), err => {
+        if (err) {
+          console.error(err);
+          res.status(502).json({ message: 'Error writing data' });
+        } else {
+          res.json(newIssue);
+        }
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
